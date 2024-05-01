08e1ba68d5731d357039fe55f2d3b5465776a039
LinnLinnHtun1197 <linlintun1197@gmail.com>
2024-05-01 11:31:41 +0800

    updated .:

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -32,6 +33,22 @@ export default function App() {
     </SafeArea>
   );
 
+  const TAB_ICON = {
+    Restaurants: "md-restaurant",
+    Map: "md-map",
+    Settings: "md-settings",
+  };
+
+
+  const createScreenOptions = ({ route }) => {
+    const iconName = TAB_ICON[route.name];
+    return {
+      tabBarIcon: ({ size, color }) => (
+        <Ionicons name={iconName} size={size} color={color} />
+      ),
+    };
+  };
+
 
   if( !oswaldLoaded || !latoLoaded) {
     return null;
@@ -41,7 +58,13 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-            <Tab.Navigator>
+          <Tab.Navigator
+              screenOptions={createScreenOptions}
+              tabBarOptions={{
+                activeTintColor: "tomato",
+                inactiveTintColor: "gray",
+              }}
+            >
               <Tab.Screen name="Restaurants" component={ RestaurantsScreen } />
               <Tab.Screen name="Map" component={Map} />
               <Tab.Screen name="Settings" component={Settings} />
